23d28f46c286e9f916f1eebfa23013862cd4b23c
Jean-Baptiste BRONISZ <jb.bronisz@ftprod.fr>
2012-10-07 18:49:28 +0200

    On supprime le sous-chemin :rails_root/public/spree du chemin d'une image stockée sur S3

--- core/app/models/spree/image.rb
+++ core/app/models/spree/image.rb
@@ -6,22 +6,28 @@ module Spree
     attr_accessible :alt, :attachment, :position, :viewable_type, :viewable_id
 
     has_attached_file :attachment,
-                      :styles => { :mini => '48x48>', :small => '100x100>', :product => '240x240>', :large => '600x600>' },
-                      :default_style => :product,
-                      :url => '/spree/products/:id/:style/:basename.:extension',
-                      :path => ':rails_root/public/spree/products/:id/:style/:basename.:extension'
+      :styles => {
+        :mini    => '48x48>',
+        :small   => '100x100>',
+        :product => '240x240>',
+        :large   => '600x600>'
+      },
+      :default_style => :product,
+      :url           => '/spree/products/:id/:style/:basename.:extension',
+      :path          => ':rails_root/public/spree/products/:id/:style/:basename.:extension'
+
     # save the w,h of the original image (from which others can be calculated)
     # we need to look at the write-queue for images which have not been saved yet
     after_post_process :find_dimensions
 
+    Spree::Image.attachment_definitions[:attachment][:styles]        = ActiveSupport::JSON.decode(Spree::Config[:attachment_styles])
+    Spree::Image.attachment_definitions[:attachment][:path]          = Spree::Config[:attachment_path]
+    Spree::Image.attachment_definitions[:attachment][:url]           = Spree::Config[:attachment_url]
+    Spree::Image.attachment_definitions[:attachment][:default_url]   = Spree::Config[:attachment_default_url]
+    Spree::Image.attachment_definitions[:attachment][:default_style] = Spree::Config[:attachment_default_style]
+
     include Spree::Core::S3Support
     supports_s3 :attachment
-
-    Spree::Image.attachment_definitions[:attachment][:styles] = ActiveSupport::JSON.decode(Spree::Config[:attachment_styles])
-    Spree::Image.attachment_definitions[:attachment][:path] = Spree::Config[:attachment_path]
-    Spree::Image.attachment_definitions[:attachment][:url] = Spree::Config[:attachment_url]
-    Spree::Image.attachment_definitions[:attachment][:default_url] = Spree::Config[:attachment_default_url]
-    Spree::Image.attachment_definitions[:attachment][:default_style] = Spree::Config[:attachment_default_style]
 
     #used by admin products autocomplete
     def mini_url
--- core/lib/spree/core/s3_support.rb
+++ core/lib/spree/core/s3_support.rb
@@ -10,13 +10,19 @@ module Spree
           # Load user defined paperclip settings
           config = Spree::Config
           if config[:use_s3]
-            s3_creds = { :access_key_id => config[:s3_access_key], :secret_access_key => config[:s3_secret], :bucket => config[:s3_bucket] }
-            self.attachment_definitions[field][:storage] = :s3
+            s3_creds = {
+              :access_key_id     => config[:s3_access_key],
+              :secret_access_key => config[:s3_secret],
+              :bucket            => config[:s3_bucket]
+            }
+            self.attachment_definitions[field][:storage]        = :s3
             self.attachment_definitions[field][:s3_credentials] = s3_creds
-            self.attachment_definitions[field][:s3_headers] = ActiveSupport::JSON.decode(config[:s3_headers])
-            self.attachment_definitions[field][:bucket] = config[:s3_bucket]
-            self.attachment_definitions[field][:s3_protocol] = config[:s3_protocol] unless config[:s3_protocol].blank?
-            self.attachment_definitions[field][:s3_host_alias] = config[:s3_host_alias] unless config[:s3_host_alias].blank?
+            self.attachment_definitions[field][:s3_headers]     = ActiveSupport::JSON.decode(config[:s3_headers])
+            self.attachment_definitions[field][:bucket]         = config[:s3_bucket]
+            self.attachment_definitions[field][:s3_protocol]    = config[:s3_protocol] unless config[:s3_protocol].blank?
+            self.attachment_definitions[field][:s3_host_alias]  = config[:s3_host_alias] unless config[:s3_host_alias].blank?
+
+            self.attachment_definitions[field][:path].gsub!(/^:rails_root\/public\/spree\//, '/')
           end
         end
       end
