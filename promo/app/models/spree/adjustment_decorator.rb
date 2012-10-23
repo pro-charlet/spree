67b56a87288d01f2f10cb1eab58d3e9c8f90b4ce
Jean-Baptiste BRONISZ <jb.bronisz@ftprod.fr>
2012-10-23 11:54:10 +0200

    Ajout du scope Spree::Adjustment.from_promotion_action qui permet de retourner les ajustements créés par l'action donnée d'une promotion

--- promo/app/models/spree/adjustment_decorator.rb
+++ promo/app/models/spree/adjustment_decorator.rb
@@ -1,7 +1,17 @@
-Spree::Adjustment.class_eval do
-  class << self
-    def promotion
-      where(:originator_type => 'Spree::PromotionAction')
-    end
+module Spree
+  Adjustment.class_eval do
+    scope :promotion, where(:originator_type => 'Spree::PromotionAction')
+
+    scope :from_promotion_action, lambda { |action|
+      adj_tbl = Adjustment.table_name
+      pa_tbl  = PromotionAction.table_name
+
+      joins(%Q{
+        INNER JOIN #{pa_tbl}
+           ON #{adj_tbl}.originator_type = 'Spree::PromotionAction' AND
+              #{adj_tbl}.originator_id = #{pa_tbl}.id}).
+      where("#{pa_tbl}.type" => action.class.name,
+            "#{pa_tbl}.id"   => action.id)
+    }
   end
 end
